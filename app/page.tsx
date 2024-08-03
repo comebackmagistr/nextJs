a121af213f045e2941dbe456045f7cee243c0a5e
Игорь Чуприков <glock.shakur@yandex.ru>
2024-08-03 23:11:44 +0300

    add header

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return <h1>
-    Home
-    <Button variant='outline'>Корзина</Button>
+    Главная
     </h1>
 }
